49eb7a358146f58c0a54254db30225165a53b2dd
Chai Keshab <keshab3886@gmail.com>
2023-11-29 16:04:34 +0545

    About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,35 +1,36 @@
-import { useNavigate } from "react-router-dom";
-import { Button } from '../components/index'
-import { filterType } from '../data/filterType'
 import { useDispatch } from "react-redux";
-import { setFilter } from '../redux/index'
+import useWindowResolution from '../hooks/useWindowResolution'
+import { RxHamburgerMenu } from "react-icons/rx";
+import { isSideBarOpen } from '../redux/index'
+
 
 const TodoApp = () => {
 
     const dispatch = useDispatch()
-    const navigate = useNavigate()
+    const windowSize = useWindowResolution();
+
+    const isLgScreen = windowSize.width >= 1024 ? true : false
 
     return (
-        <div className=" flex flex-col h-screen overflow-x-auto w-full pl-3 pb-5 mt-4 md:pl-7">
+        <div className="flex flex-col w-full p-2 mb-5">
 
-            <Button
-                onClick={() => {
-                    navigate('/')
-                    dispatch(setFilter(filterType.Todo))
-                }}
+            <div className={`${isLgScreen ? 'hidden' : 'flex'} max-h-[10vh] flex items-center py-5 justify-start`}>
+                <RxHamburgerMenu
+                    onClick={() => dispatch(isSideBarOpen())}
+                    className={`${isLgScreen ? 'hidden' : 'block'} text-xl hover:cursor-pointer`}
+                />
+                <h1 className={`mx-auto text-center text-2xl font-bold md:text-4xl`}>TODO APP</h1>
+            </div>
 
-                className=" px-4 py-2 bg-blue-400 w-fit text-white rounded-md"
-                label={'Home'}
-            />
-
-            <h1 className="text-3xl font-bold mb-4 text-center"># Simple Todo App</h1>
 
             <div className="bg-gray-100 p-6 rounded-md mb-6">
                 <h2 className="text-lg font-semibold mb-2"># Introduction</h2>
-                <p className="text-gray-700">
+                <p className="text-gray-700 text-justify">
                     This is a simple todo app designed for efficient task management. It provides an intuitive interface for creating, editing, and organizing tasks effortlessly.
                 </p>
             </div>
+
+
 
             <div className="bg-gray-100 p-6 rounded-md mb-6">
                 <h2 className="text-lg font-semibold mb-2"># Features</h2>
@@ -49,49 +50,100 @@ const TodoApp = () => {
                         <li>Vite</li>
                     </ul>
                 </div>
+
+
                 <div className="mt-4">
                     <h3 className="text-md font-semibold mb-1">Steps</h3>
                     <ol className="list-decimal list-inside text-gray-700">
                         <li>
                             Clone the repository:
-                            <pre>
-                                <code className="text-sm bg-gray-200 p-1 rounded-md">git clone https://github.com/ChaiKeshab/Todo-App.git</code>
-                            </pre>
+                            <div className=" bg-gray-200 p-2 my-1 rounded-md">
+                                <code className="text-sm">git clone https://github.com/ChaiKeshab/Todo-App.git </code>
+                            </div>
                         </li>
-                        <li>
+
+                        <li className="mt-3">
                             Install dependencies:
-                            <pre>
-                                <code className="text-sm bg-gray-200 p-1 rounded-md">
-                                    cd todo-app
+                            <div className=" bg-gray-200 p-2 my-1 rounded-md">
+
+                                <code className="text-sm">
+                                    cd [Folder location/Name]
+                                </code>
+
+                                <div></div>
+
+                                <code className="text-sm">
                                     npm install
                                 </code>
-                            </pre>
+
+                            </div>
                         </li>
-                        <li>
+
+
+
+                        <li className="mt-3">
                             Start the application:
-                            <pre>
+
+                            <ul className="my-2 list-disc list-inside text-gray-700">
+                                <li>Create a &quot;.env.local&quot; file in the root folder and copy the followings in it</li>
+                            </ul>
+
+                            <div className=" bg-gray-200 whitespace-normal break-words py-2 px-4 rounded-md">
+                                <code className="text-sm">
+                                    VITE_BASE_URL=&apos;https://jsonplaceholder.typicode.com&apos;
+                                </code>
+
+                            </div>
+
+                            <ul className="my-2 list-disc list-inside text-gray-700">
+                                <li>Run the application</li>
+                            </ul>
+
+                            <div className=" bg-gray-200 my-3 py-2 px-4 rounded-md">
                                 <code className="text-sm bg-gray-200 p-1 rounded-md">npm run dev</code>
-                            </pre>
+                            </div>
                         </li>
                     </ol>
                 </div>
             </div>
 
+
+
             <div className="bg-gray-100 p-6 rounded-md">
                 <h2 className="text-lg font-semibold mb-2"># Usage</h2>
-                <ol className="list-decimal list-inside text-gray-700">
-                    <li>
+
+                <ol className="list-decimal my-3 list-inside text-gray-700">
+                    <li className="my-4">
                         <strong>Adding a Task:</strong>
-                        <ul className="list-disc list-inside">
-                            <li>Click on the "+ Add" button.</li>
-                            <li>Enter your task in the input field and press Enter or click "Confirm" to add it to the list.</li>
+                        <ul className="mt-2 list-disc list-inside">
+                            <li>Click on the &quot;+ Add&quot; button.</li>
+                            <li>Enter your task in the input field and press Enter or click &quot;Confirm&quot; to add it to the list.</li>
+                        </ul>
+                    </li>
+
+                    <li className="my-4">
+                        <strong>Marking a Task as Complete:</strong>
+                        <ul className="mt-2 list-disc list-inside">
+                            <li>Click on the checkbox or task itself to mark it as completed.</li>
+                        </ul>
+                    </li>
+
+                    <li className="my-4">
+                        <strong>Editing or Deleting a Task:</strong>
+                        <ul className="mt-2 list-disc list-inside">
+                            <li>To edit, click on the edit icon and modify it. Press Enter or click &quot;Confirm&quot; to save changes.</li>
+                            <li>To delete, click on the delete icon next to edit button.</li>
+                            <li>To delete all, click on the &quot; - Delete All&quot; button.</li>
+                        </ul>
+                    </li>
+
+                    <li className="my-4">
+                        <strong>Additional Functionality:</strong>
+                        <ul className="mt-2 list-disc list-inside">
+                            <li>Explore additional features like filtering tasks based on status.</li>
                         </ul>
                     </li>
                 </ol>
-            </div>
-
-            <div className='text-3xl'>
-                And yes this page is GPT generated cuz i am bored now.
             </div>
         </div>
     );
